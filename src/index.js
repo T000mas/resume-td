7c26c87b7847edac4e56f40b10e80ae121f244f5
Laptop\T000mas <tomek.dominiczak@gmail.com>
2021-07-14 20:50:43 +0200

    Add changing navigation on scrolling through sections

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ReactDOM from "react-dom";
 import { HashRouter as Router, Link } from "react-router-dom";
 import Abilities from "./Abilities";
@@ -20,63 +20,78 @@ function App() {
   const abilities = useRef(null);
   const interests = useRef(null);
   const contact = useRef(null);
+  const observer = useRef(null);
 
   const executeScroll = (ref, index) => {
-    setActive(index);
     ref.current.scrollIntoView();
   };
 
-  const sections = [
-    { to: "/welcome", ref: welcome, text: "Welcome" },
-    { to: "/profile", ref: profile, text: "Profile" },
-    { to: "/experience", ref: experience, text: "Experience" },
-    { to: "/education", ref: education, text: "Education" },
-    { to: "/abilities", ref: abilities, text: "Abilities" },
-    { to: "/interests", ref: interests, text: "interests" },
-    { to: "/contact", ref: contact, text: "Contact" },
-  ];
+  const sections = useRef([
+    { to: "/welcome", ref: welcome, text: "Welcome", component: <Welcome /> },
+    { to: "/profile", ref: profile, text: "Profile", component: <Profile /> },
+    {
+      to: "/experience",
+      ref: experience,
+      text: "Experience",
+      component: <Experience />,
+    },
+    {
+      to: "/education",
+      ref: education,
+      text: "Education",
+      component: <Education />,
+    },
+    {
+      to: "/abilities",
+      ref: abilities,
+      text: "Abilities",
+      component: <Abilities />,
+    },
+    {
+      to: "/interests",
+      ref: interests,
+      text: "interests",
+      component: <Interests />,
+    },
+    { to: "/contact", ref: contact, text: "Contact", component: <Contact /> },
+  ]);
 
-  // function handleIntersect(entries, observer) {
-  //   entries.forEach((entry) => {
-  //     if (entry.intersectionRatio > prevRatio) {
-  //       entry.target.style.backgroundColor = increasingColor.replace(
-  //         "ratio",
-  //         entry.intersectionRatio
-  //       );
-  //     } else {
-  //       entry.target.style.backgroundColor = decreasingColor.replace(
-  //         "ratio",
-  //         entry.intersectionRatio
-  //       );
-  //     }
+  useEffect(() => {
+    const options = {
+      root: null,
+      rootMargin: "-35% 0px -50%",
+      threshold: 0.05,
+    };
 
-  //     prevRatio = entry.intersectionRatio;
-  //   });
-  // }
+    function handleIntersect(entries) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActive(parseInt(entry.target.id));
+        }
+      });
+    }
 
-  // function createObserver() {
-  //   let observer;
+    function createObserver() {
+      observer.current = new IntersectionObserver(handleIntersect, options);
+      sections.current.map((section) => {
+        return observer.current.observe(section.ref.current);
+      });
+    }
 
-  //   let options = {
-  //     root: null,
-  //     rootMargin: "0px",
-  //   };
+    createObserver();
 
-  //   sections.map((section) => {
-  //     observer = new IntersectionObserver(handleIntersect, options);
-  //     observer.observe(section.ref);
-  //   });
-  // }
+    return () => observer.current.disconnect();
+  }, []);
 
   return (
-    <div>
+    <div className="index">
       <Router>
         <nav>
           <ul>
-            {sections.map((section, index) => {
+            {sections.current.map((section, index) => {
               const className = active === index ? "active" : "";
               return (
-                <li key={index} className={className}>
+                <li id={active} key={index} className={className}>
                   <Link
                     to={section.to}
                     onClick={() => executeScroll(section.ref, index)}
@@ -91,27 +106,13 @@ function App() {
         </nav>
       </Router>
 
-      <div ref={welcome}>
-        <Welcome />
-      </div>
-      <div ref={profile}>
-        <Profile />
-      </div>
-      <div ref={experience}>
-        <Experience />
-      </div>
-      <div ref={education}>
-        <Education />
-      </div>
-      <div ref={abilities}>
-        <Abilities />
-      </div>
-      <div ref={interests}>
-        <Interests />
-      </div>
-      <div ref={contact}>
-        <Contact />
-      </div>
+      {sections.current.map((section, index) => {
+        return (
+          <div key={index} id={index} ref={section.ref}>
+            {section.component}
+          </div>
+        );
+      })}
     </div>
   );
 }
