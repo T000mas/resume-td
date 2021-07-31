6bac27548897d33e8bbbf38536bfd3a4635602f4
Laptop\T000mas <tomek.dominiczak@gmail.com>
2021-08-01 01:48:44 +0200

    Add progressive background image enhancement

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,15 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import imagePlaceholder from "./welcomeBackgroundPlaceholder.png";
+import fullImage from "./welcomeBackground.png";
 
 export default function Welcome() {
   const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+  const [image, setImage] = useState(`url(${imagePlaceholder})`);
 
-  function handleResize() {
-    setWindowHeight(window.innerHeight);
-  }
+  useEffect(() => {
+    function handleResize() {
+      setWindowHeight(window.innerHeight);
+    }
 
-  window.addEventListener("resize", handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="container welcome" style={{ height: windowHeight }}>
+    <div
+      className="container welcome"
+      style={{
+        height: windowHeight,
+        backgroundImage: image,
+        backgroundSize: "cover",
+        backgroundAttachment: "fixed",
+        transition: "0.5s",
+      }}
+    >
+      <img
+        src={fullImage}
+        style={{ display: "none" }}
+        alt="mlem"
+        onLoad={() => setImage(`url(${fullImage})`)}
+      />
       <div className="welcomeText">
         <div className="title">Tomasz Dominiczak</div>
         <div className="headline">Web CV</div>
